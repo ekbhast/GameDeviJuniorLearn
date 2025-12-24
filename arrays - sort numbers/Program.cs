36dbe1fe251d1ef97fa15b622cf1aefb7e1d933e
Aleksey Kirillov <alexey.kirillov@maergroup.ru>
2025-12-24 09:02:09 +0300

    Array sort

--- arrays - sort numbers/Program.cs
+++ arrays - sort numbers/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace arrays___sort_numbers
 {
@@ -10,6 +6,39 @@ namespace arrays___sort_numbers
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int maxRandomValue = 10;
+            int minRandomValue = 0;
+
+            int[] numbers = new int[10];
+
+            Console.WriteLine("Исходный массив:");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(minRandomValue, maxRandomValue + 1);
+                Console.Write(numbers[i] + " ");
+            }
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = 0; j < numbers.Length - 1 - i; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nОтсортированный массив");
+
+            for (int i = 0; i <= numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
         }
     }
 }
