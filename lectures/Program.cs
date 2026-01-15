5e014995b902d4e8b4655267a24af0b7f778311d
Aleksey Kirillov <alexey.kirillov@maergroup.ru>
2026-01-15 16:01:43 +0300

    lecture

---
+++ lectures/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace lectures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawBar(5, 10, 1, ConsoleColor.Green);
+            DrawBar(6, 10, 2, ConsoleColor.Blue);
+        }
+
+        static void DrawBar (int value, int maxValue, int position, ConsoleColor color)
+        {
+            ConsoleColor defaultColor = Console.BackgroundColor;
+            
+            Console.SetCursorPosition(0, position);
+
+            Console.Write('[');
+
+            Console.BackgroundColor = color;
+
+            for (int i = 0; i <= value; i++) 
+            {
+                Console.Write(' ');
+            }
+
+            Console.BackgroundColor = defaultColor;
+
+            for (int i = value; i <= maxValue; i++) { 
+                Console.Write(" ");
+            }
+
+            Console.WriteLine(']');
+        }
+    }
+}
---
+++ lectures/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("lectures")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lectures")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("e9187fa6-b325-49b7-90a0-9cf2358769ae")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
