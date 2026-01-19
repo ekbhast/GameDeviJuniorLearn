044af5ba92d7da258071007717ac42d532ec179d
Aleksey Kirillov <alexey.kirillov@maergroup.ru>
2026-01-19 15:21:17 +0300

    readint completed

--- UIElement/Program.cs
+++ UIElement/Program.cs
@@ -6,45 +6,62 @@ namespace UIElement
     {
         static void Main(string[] args)
         {
+            int helath = 70;
+            int healthPositionX = 1;
+            int healthPositionY = 1;
+            int healthBarLength = 33;
+
+            int mana = 68;
+            int manaPositionX = 1;
+            int manaPositionY = 3;
+            int manaBarLength = 10;
+
             Console.WriteLine("Health:");
-            Console.Write('[');
-            DrawBar(60, ConsoleColor.Green, 10, 1, 1);
-            Console.Write(']');
+            DrawBar(helath, ConsoleColor.Green, healthBarLength, healthPositionX, healthPositionY);
 
             Console.Write('\n');
 
             Console.WriteLine("Mana:");
-            Console.Write('[');
-            DrawBar(64, ConsoleColor.Blue, 10, 1, 3);
-            Console.Write(']');
+            DrawBar(mana, ConsoleColor.Blue, manaBarLength, manaPositionX, manaPositionY);
         }
 
         private static void DrawBar(int filledPercent, ConsoleColor fillColor, int barLength, int positionX, int positionY)
         {
+            const double BarPercentStep = 10.0;
+
+            int minPercentBarValue = 0;
+            int maxPercentBarValue = 100;
+
             ConsoleColor defaultColor = Console.BackgroundColor;
 
-            const int BarPercentStep = 10;
-
-            int sharsPerStep = barLength / BarPercentStep;
-            int filledSteps = filledPercent / BarPercentStep * sharsPerStep;
-
-            Console.SetCursorPosition(positionX, positionY);
-
-            for (int i = 0; i < barLength; i++)
+            if (filledPercent < minPercentBarValue || filledPercent > maxPercentBarValue || barLength <= 0)
             {
-                if (i < filledSteps)
-                {
-                    Console.BackgroundColor = fillColor;
-                    Console.Write(' ');
-                }
-                else
-                {
-                    Console.BackgroundColor = defaultColor;
-                    Console.Write('_');
-                }
+                Console.WriteLine("Произошла ошибка. Шкала имеет не допустимые значения");
             }
+            else
+            {
+                double charsPerStep = barLength / BarPercentStep;
+                double filledSteps = filledPercent / BarPercentStep * charsPerStep;
+                double remainSteps = barLength - filledSteps;
 
-            Console.BackgroundColor = defaultColor;
+                Console.SetCursorPosition(positionX, positionY);
+
+                Console.Write('[');
+                Console.BackgroundColor = fillColor;
+                DrawScale((int)Math.Round(filledSteps), ' ');
+
+                Console.BackgroundColor = defaultColor;
+                DrawScale((int)Math.Round(remainSteps), '_');
+                Console.Write(']');
+            }
+        }
+
+        private static void DrawScale(int steps, char drawChar)
+        {
+            for (int i = 0; i < steps; i++)
+            {
+                Console.Write(drawChar);
+            }
         }
     }
 }
