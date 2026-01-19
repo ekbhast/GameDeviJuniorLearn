a16639463006922a067a62a3d8144e16f9ee4146
Aleksey Kirillov <alexey.kirillov@maergroup.ru>
2026-01-19 16:06:29 +0300

    added homework shuffle

--- Shuffle/Program.cs
+++ Shuffle/Program.cs
@@ -10,11 +10,45 @@ namespace Shuffle
     {
         static void Main(string[] args)
         {
+            int[] array = new int[5];
+            int minRandomValue = 0;
+            int maxRandomValue = 100;
+
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(minRandomValue, maxRandomValue + 1);
+            }
+
+            Console.WriteLine("Исходный массив:");
+            DrawArray(array);
+
+            Shuffle(ref array, random);
+
+            Console.Write('\n');
+
+            Console.WriteLine("Измененнный массив:");
+            DrawArray(array);
         }
 
-        private static int[] Shuffle(int[] arr)
+        private static void Shuffle(ref int[] arr, Random random)
         {
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                int randomItem = random.Next(i + 1);
+                int temp = arr[i];
+                arr[i] = arr[randomItem];
+                arr[randomItem] = temp;
+            }
+        }
 
+        private static void DrawArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
         }
     }
 }
