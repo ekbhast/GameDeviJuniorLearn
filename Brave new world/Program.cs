e8961a970debd1723214ef1766e49fe0da077d5e
ekbhast <ekbhast@gmail.com>
2026-01-21 22:08:05 +0500

    Init commit Brave_new_world home work

---
+++ Brave new world/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//TO Do
+//Сделать игровую карту с помощью двумерного массива. Сделать функцию показа карты в консоли.
+//Помимо этого, дать пользователю возможность перемещаться по карте и взаимодействовать с элементами
+//(например пользователь не может пройти сквозь стену).
+//Все элементы являются обычными символами.
+//Не используйте Task.Run.
+
+namespace Brave_new_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
