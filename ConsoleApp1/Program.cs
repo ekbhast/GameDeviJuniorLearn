af31f7d9f1dceb395e68cb593f768d7048121efa
Aleksey Kirillov <alexey.kirillov@maergroup.ru>
2026-01-26 16:05:30 +0300

    added Extanded Dynamic Arrow

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int minRandomValue = 0;
+            int maxRandomValue = 100;
+
+            int purchaseCount = 10;
+
+            int account = 0;
+
+            Queue<int> amounts = new Queue<int>();
+
+            for (int i = 0; i < purchaseCount; i++)
+            {
+                amounts.Enqueue(random.Next(minRandomValue, maxRandomValue + 1));
+            }
+
+            while (amounts.Count > 0)
+            {
+                Console.Clear();
+
+                Console.WriteLine($"Ваш счет: {account} деняк\n");
+
+                Console.WriteLine("Вам принесли денежку:");
+                Console.WriteLine($"{amounts.Peek()}");               
+
+                Console.WriteLine("Нажмите любую клавишу, чтобы принять оплату и пополнить счет");
+
+                Console.ReadKey();
+                account += amounts.Dequeue();
+            }
+
+            Console.Clear();
+            Console.WriteLine("Очередь закончилась");
+        }
+    }
+}
---
+++ Extanded Dynamic Arrow/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extanded_Dynamic_Arrow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string SumCommand = "sum";
+            const string ExitCommand = "exit";
+
+            bool exit = false;
+
+            List<int> numbers = new List<int>();
+
+            while (exit == false)
+            {
+                Console.Clear();
+                Console.WriteLine($"Комманды: \n{SumCommand} - сумма всех введеных чисел \n{ExitCommand} - выход из программы\n");
+                Console.WriteLine("Введите число или команду");
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case SumCommand:
+                        Console.WriteLine("Вы ввели команду sum");
+                        Console.ReadKey();
+
+                        break;
+
+                    case ExitCommand:
+                        Console.WriteLine("Вы ввели команду exit");
+                        Console.ReadKey();
+                        break;
+
+                    default:
+                        if (int.TryParse(userInput, out int result))
+                        {
+                            numbers.Add(result);
+                            Console.WriteLine($"Число {result} добавлено \nНажмите любую клавишу для продолжнения...");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Вы ввели ни число ни команду \nНажмите любую клавишу для продолжнения... ");
+                            Console.ReadKey();
+                        }
+                            break;
+                }
+            }
+        }
+    }
+}
