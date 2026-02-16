33cd37774421563e26cbf00b5d9a5c5cc8a2d52e
Aleksey Kirillov <alexey.kirillov@maergroup.ru>
2026-02-16 15:56:12 +0300

    added classes

--- Supermarket/Program.cs
+++ Supermarket/Program.cs
@@ -6,6 +6,25 @@ using System.Threading.Tasks;
 
 namespace Supermarket
 {
+    class Supermarket
+    {
+        private Queue<Customer> _customers = new Queue<Customer>();
+        private Dictionary<string, int> _products = new Dictionary<string, int>();
+        private int _balance = 0;
+
+        public void Work()
+        {
+
+        }
+    }
+
+    class Customer
+    {
+        private int _balance;
+        private Dictionary <string, int> _cart = new Dictionary<string, int>();
+        private Dictionary <string, int> _bag = new Dictionary<string, int>();
+    }
+
     internal class Program
     {
         static void Main(string[] args)
