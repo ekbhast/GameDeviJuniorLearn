010235ddffe662d051f090c4bce990c2567e21ed
Aleksey Kirillov <alexey.kirillov@maergroup.ru>
2026-03-06 16:16:03 +0300

    dev

---
+++ 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Player> players = new List<Player>()
+            {
+                new Player("Джон", 100),
+                new Player("Билл", 210),
+                new Player("Дерек", 3110),
+                new Player("Klark", 2340)
+            };
+
+            var newPlayers = from Player player in players select new { Name = player.Name, dateOfBirth = DateTime.Now };
+            var newPlayers2 = players.Select(player => new { Name = player.Name, dateOfBirth = DateTime.Now });
+        }
+    }
+
+    class Player
+    {
+        public string Name { get; private set; }
+        public int Level { get; private set; }
+
+        public Player(string login, int level)
+        {
+            Name = login;
+            Level = level;
+        }
+    }
+}
