0a9808c7362ff9dd179bb21e4a6c073d182164f0
Alexey Kirillov <ekbhast@yandex.ru>
2026-08-17 17:53:43 +0500

    second commit

--- RememberAll/RememberAll/Program.cs
+++ RememberAll/RememberAll/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿namespace RememberAll
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string message = "Привет, у нас все получилось!";
+            Console.WriteLine($"{message}");
+        }
+    }
+}
